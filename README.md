b6e2ee74fdb5e1cf40ab9fa406035f2fcee378cd
Caleb Rodriguez <romariocalebrc@gmail.com>
2024-02-29 12:53:16 -0500

    Creación del proyecto

---
+++ README.md
@@ -0,0 +1,7 @@
+# Equipo02Hotel - Backend
+
+Ejecutar el siguiente código para generar dependencias
+
+```
+maven clean
+maven install
---
+++ src/main/java/com/equipo02/hotel/Equipo02HotelApplication.java
@@ -0,0 +1,13 @@
+package com.equipo02.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipo02HotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo02HotelApplication.class, args);
+	}
+
+}
